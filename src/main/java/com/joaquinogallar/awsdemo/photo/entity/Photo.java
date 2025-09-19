bccb6b58adb7cf17230c5a202425cd1cbeae4df4
Joaquin Ogallar <joaquinnogallar@gmail.com>
2025-09-18 22:33:50 -0300

    feat: added db dependencies

---
+++ src/main/java/com/joaquinogallar/awsdemo/photo/entity/Photo.java
@@ -0,0 +1,23 @@
+package com.joaquinogallar.awsdemo.photo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Photo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+}
